b5333fecf0a51c48a8e05b9afca5b0180a29a87c
Mitchell Henke <mitchh23@gmail.com>
2017-01-25 09:41:44 -0600

    rename script

--- script.exs
+++ script.exs
@@ -10,7 +10,19 @@ months = %{
   "Apr" => "04",
   "Mar" => "03",
   "Feb" => "02",
-  "Jan" => "01"
+  "Jan" => "01",
+  "December" => "12",
+  "November" => "11",
+  "October" => "10",
+  "September" => "09",
+  "August" => "08",
+  "July" => "07",
+  "June" => "06",
+  "May" => "05",
+  "April" => "04",
+  "March" => "03",
+  "February" => "02",
+  "January" => "01"
 }
 
 get_messages_post_private_channels = fn(x) ->
@@ -40,12 +52,19 @@ emails = File.read!("MilwaukeeSlackData.mbox")
 # open new csv file
 file = File.open!("data.csv", [:write])
 # write csv header
-IO.binwrite(file, "date,total,public,private,direct,files\n")
+IO.binwrite(file, "date,total,public,private,direct,users,files\n")
 # for each email, get date and relevant stats, and write to the csv file
 Enum.each(emails, fn(x) ->
-  [_, day, month, year] = Regex.run(~r/Date:.* (\d{1,2}) (\w{3}) (\d{4})/, x)
-  date = "#{year}-#{Map.fetch!(months, month)}-#{String.pad_leading(day, 2, "0")}"
-  IO.inspect date
+  [_, _day, month, year] = Regex.run(~r/Date:.* (\d{1,2}) (\w{3}) (\d{4})/, x)
+  [_, _, _, month2, day2] = Regex.run(~r/\w+, (\w+) (\d+)\w+ - \w+, (\w+) (\d+)\w+/, x)
+
+  year = if(month == "Jan" && month2 == "December") do
+    Integer.to_string((String.to_integer(year) - 1))
+  else
+    year
+  end
+
+  date = "#{year}-#{Map.fetch!(months, month2)}-#{String.pad_leading(day2, 2, "0")}"
 
   [_, total] = Regex.run(~r/Your team sent a total of (\d{0,2},{0,1}\d{3}) messages last week/, x)
   total = String.replace(total, ",", "") |> String.to_integer
@@ -57,9 +76,10 @@ Enum.each(emails, fn(x) ->
   end
   [_, files] = Regex.run(~r/(\d{1,3}) files/, x)
   files = String.to_integer(files)
+  [_, users]  = Regex.run(~r/there are (\d+) people/, x)
 
 
-  data = ["#{date}", total, public, private, direct, files]
+  data = ["#{date}", total, public, private, direct, users, files]
 
   data = Enum.join(data, ",") <> "\n"
   IO.binwrite(file, data)
